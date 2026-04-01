a817d70213185d8f37a594752d205ed780c44e0c
Oleg781 <oleg.fedjuk@gmail.com>
2026-04-01 13:22:36 +0300

    update

--- keikkainfo2(Node-express)/backend/db.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const knex = require('knex');
-const { DATABASE_OPTIONS } = require('./utils/config');
-console.log(DATABASE_OPTIONS);
-const db = knex(DATABASE_OPTIONS);
-
-module.exports = db;
--- keikkainfo2(Node-express)/backend/utils/dbConneciton.js
+++
@@ -1,4 +0,0 @@
-const config = require('./config.js')
-const options = config.DATABASE_OPTIONS;
-const db = require('knex')(options)
-module.exports = db;
